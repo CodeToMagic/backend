af95b8f4cb83c6988eb4a6a29d48a5226ef842d9
Buddi Navaneeth Kumar <35196622+Nava308@users.noreply.github.com>
2023-12-07 20:45:38 -0600

    added isAuthenticated route

--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import { login, logout, register } from "../services/authentication";
 import { isAuthenticated } from "../middlewares";
+import { get } from "lodash";
 
 export default (router: express.Router) => {
   router.post("/auth/register", register);
   router.post("/auth/login", login);
-  router.get("/auth/logout",isAuthenticated,logout);
+  router.get("/auth/logout", isAuthenticated, logout);
+  router.get("/auth/isAuthenticated", isAuthenticated, (req: express.Request, res: express.Response) => {
+    const userInfo = get(req, "identity.userRole");
+    return res.status(200).json({
+      userInfo
+    });
+  })
 };
--- src/services/medicines.ts
+++ src/services/medicines.ts
@@ -1,63 +1,74 @@
 import express from "express";
 import { INVALID_REQUEST, MEDICINE_DELETE_SUCCESS, MEDICINE_SUCCESS, MEDICINE_UPDATE_SUCCESS, SYSTEM_ERROR } from "../helpers/constants";
 import { createMedicineSchema, updateMedicineSchema } from "../helpers/validations";
-import { createMedicineDB,deleteMedicineDB,updateMedicineDB } from "../db/medicines/medicines";
+import { createMedicineDB, deleteMedicineDB, updateMedicineDB } from "../db/medicines/medicines";
 
-export const createMedicine =async(req: express.Request, res: express.Response) => {
+export const createMedicine = async (req: express.Request, res: express.Response) => {
     try {
         const valid = createMedicineSchema.validate(req.body);
 
         if (valid.error) {
-          return res.status(400).json({ errorMessage: INVALID_REQUEST });
+            return res.status(400).json({ errorMessage: INVALID_REQUEST });
         }
 
         await createMedicineDB(req?.body);
         return res.status(200).json({
             successMessage: MEDICINE_SUCCESS,
-          });
+        });
     } catch (error) {
         return res.status(500).json({
             errorMessage: SYSTEM_ERROR,
             systemError: error,
-          });
+        });
     }
 }
 
-export const updateMedicine =async(req: express.Request, res: express.Response) => {
+export const updateMedicine = async (req: express.Request, res: express.Response) => {
     try {
         const valid = updateMedicineSchema.validate(req.body);
-        const medicineId = parseInt(req?.params?.medicineId,10);
-        if (!medicineId||valid.error) {
-          return res.status(400).json({ errorMessage: INVALID_REQUEST });
+        const medicineId = parseInt(req?.params?.medicineId, 10);
+        if (!medicineId || valid.error) {
+            return res.status(400).json({ errorMessage: INVALID_REQUEST });
         }
-        await updateMedicineDB(medicineId,req?.body);
+        await updateMedicineDB(medicineId, req?.body);
         return res.status(200).json({
             successMessage: MEDICINE_UPDATE_SUCCESS,
-          });
+        });
     } catch (error) {
         return res.status(500).json({
             errorMessage: SYSTEM_ERROR,
             systemError: error,
-          });  
+        });
     }
-} 
+}
 
-export const deleteMedicine =async (req: express.Request, res: express.Response) => {
+export const deleteMedicine = async (req: express.Request, res: express.Response) => {
     try {
-        const medicineId = parseInt(req?.params?.medicineId,10);
-        if(!medicineId){
+        const medicineId = parseInt(req?.params?.medicineId, 10);
+        if (!medicineId) {
             return res.status(400).json({ errorMessage: INVALID_REQUEST });
         }
         await deleteMedicineDB(medicineId);
         return res.status(204).json({
             successMessage: MEDICINE_DELETE_SUCCESS,
-          });
-        
+        });
+
     } catch (error) {
         return res.status(500).json({
             errorMessage: SYSTEM_ERROR,
             systemError: error,
-          });  
+        });
     }
-    
+
+}
+
+export const getAllMedicines = async (req: express.Request, res: express.Response) => {
+    try {
+        // const res
+    } catch (error) {
+        return error.status(500).json({
+            errorMessage: SYSTEM_ERROR,
+            systemError: error,
+        });
+    }
 }
